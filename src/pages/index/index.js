ce959f82cc95031b7b14337eb34736f9b5853148
kingsley <kingsley2036@qq.com>
2019-10-31 00:57:56 +0800

    '首页'

---
+++ src/pages/index/index.js
@@ -0,0 +1,23 @@
+import 'css/common.css'
+import './index.css'
+import Vue from 'vue'
+import axios from 'axios'
+import url from '../../modules/js/api.js'
+
+
+let app=new Vue({
+  el: '#app',
+  data: {
+    lists: []
+  },
+  created() {
+    axios.post(url.hotLists, {
+      pageNum: 1,
+      pageSize: 5
+    }).then((res) => {
+      this.lists = res.data.lists
+    })
+
+  }
+
+})
